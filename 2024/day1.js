8db2393912a3bf1b8d683178318aad0d790729c5
John Holbrook <3682581+johnholbrook@users.noreply.github.com>
2024-12-01 10:07:42 -0500

    Shorten 2024 day 1

--- 2024/day1.js
+++ 2024/day1.js
@@ -1,27 +1,12 @@
 const utils = require("../utils.js");
 
-let input = utils.read_file("day1.in");
+let input = utils.read_file("day1.in").split('\n').map(utils.get_numbers);
 
-let list1 = [];
-let list2 = [];
-
-input.split('\n').forEach(line => {
-    let nums = utils.get_numbers(line);
-    list1.push(nums[0]);
-    list2.push(nums[1]);
-});
-
-list1.sort((a,b) => a-b);
-list2.sort((a,b) => a-b);
+let list1 = input.map(r => r[0]).sort((a,b) => a-b);
+let list2 = input.map(r => r[1]).sort((a,b) => a-b);
 
 let part1 = list1.reduce((t, v, i) => t + Math.abs(v - list2[i]), 0);
 console.log("Part 1:", part1);
 
 let part2 = list1.reduce((t, v) => t + (v * utils.count_occurrences_in_array(list2, v)), 0)
 console.log("Part 2:", part2);
-
-// let differences = list1.map((v, i) => Math.abs(v - list2[i]));
-// console.log("Part 1:", utils.sum_list(differences));
-
-// let similarity_scores = list1.map(v => v * utils.count_occurrences_in_array(list2, v))
-// console.log("Part 2:", utils.sum_list(similarity_scores));
